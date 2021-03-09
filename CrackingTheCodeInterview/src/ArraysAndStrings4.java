b0df5e6b06b66fb912ced298c84e99daa1172b88
Szmecskas Lidia <lidia.szm@gmail.com>
2021-03-09 08:38:37 +0200

    Arrays and Strings 1.4

---
+++ CrackingTheCodeInterview/src/ArraysAndStrings4.java
@@ -0,0 +1,38 @@
+
+public class ArraysAndStrings4 {
+	private String str="";
+    public ArraysAndStrings4(String str) {
+		this.str = str;
+    }
+
+    //Varianta 1
+    public boolean isPermutationOfPalindrome(String str) {
+    	int odd=0;
+		int even=0;
+    	int[] s=new int[128];
+    	for(int i=0;i<str.length();i++)
+    		s[str.charAt(i)]++;
+    	for(int i=0;i<s.length;i++) {
+    		if(s[i]%2==0)
+    			even++;
+    		else {
+    			odd++;
+    			if(odd>1)
+    				return false;
+    		}
+    	}
+    	return true;
+    }
+    
+    //Varianta 2
+    public boolean isPermutationOfPalindrome1(String str) {
+    	int bit=0;
+    	for(char c:str.toCharArray())
+    		bit <<= Character.getNumericValue(c);
+    	int b=bit;
+    	bit -= 1;
+    	return ((bit&b)==0);
+    }
+}
+
+
